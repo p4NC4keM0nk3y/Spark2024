3b4663686d01dceda6d2369ac66910cae84b53fb
Micah Bragg <micahabragg@gmail.com>
2024-03-15 11:21:41 -0400

    API working, no errors on run

--- main.py
+++ main.py
@@ -6,46 +6,52 @@ from reportlab.lib.pagesizes import letter
 from pathlib import Path
 import base64
 import json
+from anthropic import Anthropic
+from dotenv import load_dotenv
+import os
+api_key="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"
+
+client = Anthropic(api_key=api_key)
 
 
-with open("config.json") as config_file:
-    config = json.load(config_file)
-    api_key = config["api_key"]
 
-client = Anthropic(api_key)
 image_path = "/Users/micahbragg/Desktop/cs shit/spark-2024/Helloworld.png"
-with Image.open(image_path) as image: 
+
+with Image.open(image_path) as image:
     buffered = BytesIO()
     image.save(buffered, format="PNG")
     encoded_string = base64.b64encode(buffered.getvalue()).decode()
-response = client.messages.create(
+
+message = client.messages.create(
+    model="claude-3-opus-20240229",
     max_tokens=1024,
     messages=[
         {
             "role": "user",
             "content": [
                 {
-                    "type": "text",
-                    "text": "Hello!",
-                },
-                {
                     "type": "image",
                     "source": {
                         "type": "base64",
-                        "media_type": "image/png",
+                        "media_type": 'image/png',
                         "data": encoded_string,
                     },
                 },
+                {
+                    "type": "text",
+                    "text": "Describe this image."
+                }
             ],
-        },
+        }
     ],
-    model="claude-3-opus-2024-0229",
 )
-extracted_text = response.model_dump_json(["messages"][0]["content"][0]["text"])
+
+extracted_text = message.model_dump_json()["messages"][0]["content"][0]["text"]
+
 pdf_path = "/Users/micahbragg/Desktop/cs shit/spark-2024/TestPDF"
 c = canvas.Canvas(pdf_path, pagesize=letter)
-c.drawString(100,750,extracted_text)
+c.drawString(100, 750, extracted_text)
 c.showPage()
 c.save()
+
 print(f"Extracted text has been written {pdf_path}")
-##print(response.model_dump_json(indent=2))
